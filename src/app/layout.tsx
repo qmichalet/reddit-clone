529446481084ccf65eb7b419e8c3004ddcc99787
Quentin Michalet <qmichalet@mac-mini-de-quentin.home>
2023-08-15 01:17:48 +0200

    Feature : handle auth ui and layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { cn } from "@/lib/utils";
 import "@/styles/globals.css";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
+import { Toaster } from "@/components/ui/toaster";
 
 export const metadata = {
   title: "Breadit",
@@ -25,8 +26,11 @@ export default function RootLayout({
     >
       <body className="min-h-screen pt-12 bg-slate-50 antialiased">
         <Navbar />
-        <div className="container max-w-7xl mx-auto h-full pt-12"></div>
-        {children}
+        <div className="container max-w-7xl mx-auto h-full pt-12">
+          {children}
+        </div>
+
+        <Toaster />
       </body>
     </html>
   );
