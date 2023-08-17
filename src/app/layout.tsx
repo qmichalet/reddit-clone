cc6b8e3dac68e32003ecc53f8e67661f86809f52
Quentin Michalet <qmichalet@mac-mini-de-quentin.home>
2023-08-17 02:48:24 +0200

    Feature : create subreddit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,17 @@
-import { cn } from "@/lib/utils";
-import "@/styles/globals.css";
-import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
-import { Toaster } from "@/components/ui/toaster";
+import { cn } from "@/lib/utils";
+import { Inter } from "next/font/google";
+import Providers from "@/components/Providers";
+import { Toaster } from "@/components/ui/Toaster";
+
+import "@/styles/globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Breadit",
   description: "A Reddit clone built with Next.js and TypeScript."
 };
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
@@ -27,13 +29,15 @@ export default function RootLayout({
       )}
     >
       <body className="min-h-screen pt-12 bg-slate-50 antialiased">
-        {/* @ts-expect-error server component */}
-        <Navbar />
-        {authModal}
-        <div className="container max-w-7xl mx-auto h-full pt-12">
-          {children}
-        </div>
+        <Providers>
+          {/* @ts-expect-error Server Component */}
+          <Navbar />
+          {authModal}
 
+          <div className="container max-w-7xl mx-auto h-full pt-12">
+            {children}
+          </div>
+        </Providers>
         <Toaster />
       </body>
     </html>
