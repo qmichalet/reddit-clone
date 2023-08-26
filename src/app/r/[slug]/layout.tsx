e47f699aff1ec5687ac515aa392bf4e712844bfa
Quentin Michalet <qmichalet@mac-mini-de-quentin.home>
2023-08-26 02:42:07 +0200

    Feature : post details page

--- src/app/r/[slug]/layout.tsx
+++ src/app/r/[slug]/layout.tsx
@@ -51,7 +51,7 @@ const Layout = async ({
           {/* info sidebar */}
           <div className="hidden md:block overflow-hidden h-fit rounded-lg border border-gray-200 order-first md:order-last">
             <div className="px-6 py-3">
-              <p className="font-semibold py-3">About r/</p>
+              <p className="font-semibold py-3">About r/{subreddit.name}</p>
             </div>
 
             <dl className="divide-y divide-gray-100 px-6 py-4 text-sm leading-6 bg-white">
